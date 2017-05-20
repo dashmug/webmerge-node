929bf3a71e38847f20fe79ef80a312a7d99e2ccb
Noel Martin Llevares <noelmartin@gmail.com>
2017-05-21 05:34:28 +1000

    Docs plus linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,11 @@ module.exports = {
   ],
   rules: {
     'max-len': ['error', 120],
+    'no-useless-constructor': 'off',
+    'semi': ['error', 'never'],
     'jest/no-disabled-tests': 'warn',
     'jest/no-focused-tests': 'error',
     'jest/no-identical-title': 'error',
     'jest/valid-expect': 'error',
-  }
-};
+  },
+}
--- __mocks__/request.js
+++ __mocks__/request.js
@@ -1,11 +1,11 @@
-'use strict';
+'use strict'
 
-const request = jest.genMockFromModule('request');
+const request = jest.genMockFromModule('request')
 
 request.defaults = jest.fn(() => ({
   get: jest.fn((opts, callback) => callback(null, {}, {})),
   post: jest.fn((opts, callback) => callback(null, {}, {})),
   put: jest.fn((opts, callback) => callback(null, {}, {})),
-}));
+}))
 
-module.exports = request;
+module.exports = request
